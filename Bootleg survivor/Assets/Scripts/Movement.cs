10b8aa65940cbfcaa7dc3c5f69bfa0d715a09082
Ragglepop <cvdota@gmail.com>
2024-03-30 12:00:19 +0200

    Normalized movement
    Normalized movement, so that going diagonally is not quicker than going horizontally/vertically.

--- Bootleg survivor/Assets/Scripts/Movement.cs
+++ Bootleg survivor/Assets/Scripts/Movement.cs
@@ -6,12 +6,21 @@ public class Movement : MonoBehaviour
 {
     public GameObject camera;
     public float speed;
-    private float xInput, yInput;
+    private float axisRatio;
+
+    private void Start(){
+        axisRatio = Mathf.Sqrt(1f/2f);
+    }
 
     private void FixedUpdate(){
-        xInput = Input.GetAxis("Horizontal");
-        yInput = Input.GetAxis("Vertical");
+        float xMultiplier = Input.GetAxis("Horizontal");
+        float yMultiplier = Input.GetAxis("Vertical");
 
-        camera.transform.Translate(xInput*speed, yInput*speed, 0);
+        if(xMultiplier!=0 && yMultiplier!=0){//Normalize values if going diagonally
+            xMultiplier *= axisRatio;
+            yMultiplier *= axisRatio;
+        }
+
+        camera.transform.Translate(xMultiplier*speed, yMultiplier*speed, 0);
     }
 }
